ec0dc8d35dababc578631ccad8a946637bce26c5
Joey Lawter <42898462+Lodisy12@users.noreply.github.com>
2019-09-15 10:39:02 -0700

    Button name added Button event press added On click listener added

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.example.diceroller
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val roll6Button: Button = findViewById(R.id.roll_1d6_button)
+        roll6Button.text = "Let's Roll"
+        roll6Button.setOnClickListener { Toast.makeText(this, "Button Clicked", Toast.LENGTH_SHORT).show() }
     }
 }
