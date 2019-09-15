dc9493bc95b0acf51b1bcdbbf12e6da9bc410ef1
Joey Lawter <42898462+Lodisy12@users.noreply.github.com>
2019-09-15 11:18:01 -0700

    Removed Toast from D6 button Added text change to D6 button ("Dice Rolled!")

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -4,7 +4,9 @@ import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
+import kotlinx.android.synthetic.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,6 +15,14 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val roll6Button: Button = findViewById(R.id.roll_1d6_button)
         roll6Button.text = "Let's Roll"
-        roll6Button.setOnClickListener { Toast.makeText(this, "Button Clicked", Toast.LENGTH_SHORT).show() }
+        roll6Button.setOnClickListener {
+            rollDice()
+
+        }
+    }
+
+    private fun rollDice() {
+        val resultText: TextView = findViewById(R.id.d6Result_text)
+        resultText.text = "Dice Rolled!"
     }
 }
